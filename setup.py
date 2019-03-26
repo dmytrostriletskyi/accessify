e71a35da9419aaf91a6dc75ea12811fab1ed338b
Dmytro Striletskyi <dmytro.striletskyi@gmail.com>
2019-03-27 00:23:41 +0200

    Bump project version to 0.3.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md', 'r') as read_me:
     long_description = read_me.read()
 
 setup(
-    version='0.3.0',
+    version='0.3.1',
     name='accessify',
     description=DESCRIPTION,
     long_description=long_description,
